e1b8cf4be01f25517ece857207d5a5f4b995663c
Tony_XLZ <zhenxun.wu.23@ucl.ac.uk>
2024-12-01 00:14:46 +0000

    add localhost for test

--- Portfolio_with_Django/settings.py
+++ Portfolio_with_Django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-$0-z==m@!6%d+3)=l3c8djaeg_qz#avbawa9lzsag98e#!juox
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['134.209.177.229', 'tonywu.info', 'www.tonywu.info']
+ALLOWED_HOSTS = ['134.209.177.229', 'tonywu.info', 'www.tonywu.info', '127.0.0.1']
 
 
 # Application definition
